b9245deb414bac30f2fac9397e2038b355cadafd
Blurredbits <mmorris00@mac.com>
2015-11-11 19:31:39 -0700

    Stopping point for the day...

---
+++ chapter_3_calc/exercise_10/ex10_self_checkout.js
@@ -0,0 +1,4 @@
+// Create a simple self-checkout system. Prompt for the prices
+// and quantities of three items. Calculate the subtotal of the items.
+// Calculate the tax using a rate of 5.5%. Print out the the line items
+// with the quantity and total, and then print out the subtotal, tax amount and total.
